4ce5b2107d70a67e4130ad6a4f7ee4e7a80083c7
Quderlo <60547547+Quderlo@users.noreply.github.com>
2020-05-30 15:18:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import logging
 import random
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
-
+import ephem
+from datetime import datetime, date, time
 
 PROXY = {'proxy_url': settings.PROXY_URL,
     'urllib3_proxy_kwargs': {
@@ -12,19 +13,36 @@ PROXY = {'proxy_url': settings.PROXY_URL,
 }
 
 def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(user_text)
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
 
 def greet_user(update, context):
     print('Вызван /start')
     update.message.reply_text('Привет!')
-    update.message.reply_text('Список команд: \n 1) /start \n 2) /joke \n 3) /iq ')
+    update.message.reply_text('Список команд: \n 1) /start \n 2) /planet \n 3) /joke \n 4) /iq ')
 
 def joke_user(update, context):
     update.message.reply_text('Знаете почему нельзя шутить про подлодку Курск?')
     update.message.reply_text('Потому что в материале слишком много воды')
 
+def planet_user(update, context):
+	text = update.message.text.split()
+	del text[0]
+	if text[0] == 'mars':
+		mars = ephem.Mars('2000/01/01')
+		constellation = ephem.constellation(mars)
+		print(constellation)
+		for i in constellation:
+			update.message.reply_text(i)
+	elif text[0] == 'moon':
+		moon = ephem.Moon('2000/01/01')
+		constellation = ephem.constellation(moon)
+		print(constellation)
+		for i in constellation:
+			update.message.reply_text(i)
+	else:
+		update.message.reply_text('Я пока не знаю такую планету, прости')
 
 def iq_user(update, context):
     update.message.reply_text('Твой iq = ' + str(random.randint(1,200)))
@@ -38,6 +56,7 @@ def main():
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("joke", joke_user))
     dp.add_handler(CommandHandler("iq", iq_user))
+    dp.add_handler(CommandHandler("planet", planet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('ВКЛ')
